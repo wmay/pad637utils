0b6df0612e6f5fddcdaaf6d4980cbeabed46887b
William May <williamcmay@live.com>
2016-05-08 17:36:15 -0400

    Get friends by default, added error-handling function

--- R/utils.R
+++ R/utils.R
@@ -10,6 +10,17 @@
   packageStartupMessage(wmtext)
 }
 
+handle_errors = function(account, method) {
+  # try to handle the API error responses somewhat gracefully
+  if (method == "friends") {
+    suppressWarnings(try(account$getFriendIDs(retryOnRateLimit = 20),
+                         silent = T))
+  } else {
+    suppressWarnings(try(account$getFollowerIDs(retryOnRateLimit = 20),
+                         silent = T))
+  }
+}
+
 
 #' Construct a Twitter follower matrix
 #'
@@ -17,6 +28,9 @@
 #' 
 #' @param users A character vector of Twitter handles (with no '@'
 #' symbol) or user IDs
+#' @param method Construct the matrix by gathering either "friends" or
+#' "followers" data for each account. "friends" will be faster for
+#' accounts with more followers than friends, and vice versa.
 #' @return A square matrix representing the directed graph of twitter
 #' follower relationships. Each entry in the matrix is 1 if the row
 #' account is following the column account, otherwise 0.
@@ -28,24 +42,29 @@
 #'     access_secret = "[your info]")
 #' users = c("c4ssdotorg", "RoderickTLong", "AynRandInst", "AynRandOrg")
 #' m = follower_matrix(users)
+#'
+#' # plot with igraph
+#' library(igraph)
+#' g = graph_from_adjacency_matrix(m, "directed")
+#' plot(g)
 #' }
 #' @export
-follower_matrix = function(users) {
+follower_matrix = function(users, method = "friends") {
   # get user info
   n_users = length(users)
   r1 = lookupUsers(users)
 
   # collect the followers of each user
   followers = list()
-  for (account in r1) {
+  for (n in 1:length(r1)) {
+    account = r1[n][[1]]
     handle = account$screenName
-    cat(paste0("Collecting @", handle,
-               "'s followers...\n"))
+    cat(paste0("Collecting @", handle, "'s ", method, "...\n"))
 
     # If the user's friends are private, skip to the next one
     if (account$protected) {
-      cat(paste0("Warning: @", handle,
-                 "'s followers are private and will not be returned by the API"))
+      cat(paste0("Warning: @", handle, "'s ", method,
+                 " are private and will not be returned by the API"))
       followers[[handle]] = NULL
       next
     }
@@ -54,17 +73,17 @@ follower_matrix = function(users) {
     # https://github.com/geoffjentry/twitteR/blob/master/R/comm.R
     # If I get a weird 'try-error', wait for a bit
     n_errors = 0
-    while(class(followers[[handle]] <- 
-                  try(account$getFollowerIDs(retryOnRateLimit = 20),
-                      silent = T)) == "try-error") {
+    followers[[handle]] = handle_errors(account, method)
+    while(class(followers[[handle]]) == "try-error") {
       
       # give up after 20 tries
       if (n_errors > 20) stop("Could not get data from Twitter.\n")
-
-      # wait and hope it gets better
-      cat("Waiting on the API...\n")
+      
+      # wait and try again
+      cat("Blocked due to rate limit. Waiting 1 minute...\n")
       n_errors = n_errors + 1
       Sys.sleep(60)
+      followers[[handle]] = handle_errors(account, method)
     }
   }
 
@@ -82,11 +101,23 @@ follower_matrix = function(users) {
   # convert ids to names
   followdf$name = userdf$name[match(followdf$value, userdf$id)]
   # fill up the matrix
-  mat[cbind(followdf$name, followdf$L1)] = 1
-  # remove values for private accounts
-  for (account in r1) {
-    handle = account$screenName
-    if (is.null(followers[[handle]])) mat[handle, ] = NA
+  if (method == "friends") {
+    mat[cbind(followdf$L1, followdf$name)] = 1
+    # remove values for private accounts
+    for (n in 1:length(r1)) {
+      account = r1[n][[1]]
+      handle = account$screenName
+      if (is.null(followers[[handle]])) mat[handle, ] = NA
+    }
+  } else {
+    mat[cbind(followdf$name, followdf$L1)] = 1
+    # remove values for private accounts
+    for (n in 1:length(r1)) {
+      account = r1[n][[1]]
+      handle = account$screenName
+      if (is.null(followers[[handle]])) mat[, handle] = NA
+    }
   }
+
   mat
 }
